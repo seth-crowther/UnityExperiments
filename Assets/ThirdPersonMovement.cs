127592e35353e220a15d357ceb485af65867daed
Seth Crowther <sethcrowther9@gmail.com>
2023-01-20 19:07:07 +0000

    Movement script is now tracked.

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public Transform mainCam;
+    public CharacterController controller;
+
+    public float speed = 6.0f;
+    public float turnSmoothTime = 0.1f;
+    public float jumpSpeed = 0.5f;
+
+    float turnSmoothVelocity;
+    private float distToGround;
+    private float ySpeed;
+
+    private void Start()
+    {
+        distToGround = controller.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get horizontal and vertical input. "GetAxisRaw" means no input smoothing.
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (!IsGrounded())
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            ySpeed = jumpSpeed;
+        }
+        else
+        {
+            ySpeed = 0;
+        }
+
+        if (direction.magnitude >= 0.1f) // If there is some direction input
+        {
+            // Calculating desired angle for character to face forward
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCam.eulerAngles.y;
+
+            // Smooths turning angle so the target angle is reached in turnSmoothTime seconds
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Vector3 movement = speed * Time.deltaTime * moveDir.normalized;
+            controller.Move(movement);
+        }
+
+        transform.position += new Vector3(0f, ySpeed, 0f) * Time.deltaTime;
+
+    }
+
+    public bool IsGrounded() // Doesn't necessarily work, what if capsule is sitting on a small hole. Fix later.
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
