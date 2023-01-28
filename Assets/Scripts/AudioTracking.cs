08e118842cdc0f94d5c0bf1567bd0443093cf03f
Seth Crowther <sethcrowther9@gmail.com>
2023-01-28 20:12:17 +0000

    Worked on getting speaker attenuation/pan working right. Very tricky.

--- Assets/Scripts/AudioTracking.cs
+++ Assets/Scripts/AudioTracking.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class AudioTracking : MonoBehaviour
 {
-    public AudioListener playerListener;
-    public AudioSource audioSource;
+    public Transform mainCam;
+    public Transform player;
+    public float angleToSpeaker;
+
+    private Vector3 cameraForward;
+    private Vector3 dirToSpeaker;
+    private AudioSource audioSource;
+
+    private float range;
+    private float playerDist;
+    private float maxVolume = 0.1f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        range = audioSource.maxDistance;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AdjustAudioPan();
+        AdjustAudioAttenuation();
+    }
+
+    private void AdjustAudioPan() // Doesn't work!
+    {
+        cameraForward = new Vector3(mainCam.transform.forward.x, 0f, mainCam.transform.forward.z).normalized;
+        dirToSpeaker = (transform.position - new Vector3(mainCam.transform.position.x, 0f, mainCam.transform.position.z)).normalized;
+
+        angleToSpeaker = Vector3.SignedAngle(cameraForward, dirToSpeaker, Vector3.up) * Mathf.Deg2Rad;
+        audioSource.panStereo = Mathf.Cos(angleToSpeaker);
+    }
+
+    private void AdjustAudioAttenuation() // Doesn't work!
+    {
+        playerDist = Vector3.Distance(transform.position, player.position);
+        audioSource.volume = -(1 / range * maxVolume) + maxVolume;
     }
 }
