833fafef8dba8c4c5dc3a5ba2a31a4398c60b013
Seth Crowther <sethcrowther9@gmail.com>
2023-04-06 14:39:55 +0100

    Camera now moves horizontally as intended over the shoulder. Need to stop camera snapping behind player when vertical limit is reached.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,17 +6,22 @@ public class MouseLook : MonoBehaviour
 {
     public float sensitivity = 2.0f;
     public Transform player;
+    public Transform mainCam;
+    public Vector3 playerOffset;
 
-    private void Start()
+    void Start()
     {
-        
+        playerOffset = transform.position - player.position;    
     }
     void Update()
     {
         float horizontal = Input.GetAxis("Mouse X");
         float vertical = Input.GetAxis("Mouse Y");
-        Vector3 rotation = new Vector3(-vertical, horizontal, 0) * sensitivity;
+        //Vector3 rotation = new Vector3(-vertical, horizontal, 0) * sensitivity;
 
-        transform.Rotate(rotation);
+        transform.Rotate(new Vector3(-vertical, 0, 0) * sensitivity);
+        transform.RotateAround(player.position, player.up, horizontal * sensitivity);
+
+        //transform.position = player.position + playerOffset;
     }
 }
